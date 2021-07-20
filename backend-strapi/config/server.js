61afac8b622e8860a24648911cc07fe752f7f58e
visualgravitysense <helikeel@gmail.com>
2021-07-20 15:09:43 +0300

    Kangelane Development: Adding backend to host

--- backend-strapi/config/server.js
+++ backend-strapi/config/server.js
@@ -1,9 +1,14 @@
+// module.exports = ({ env }) => ({
+//   host: env('APP_HOST', '0.0.0.0'),
+//   port: env.int('NODE_PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', '83c53a97d7567df38d9e40c4fb4fb4b2'),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '83c53a97d7567df38d9e40c4fb4fb4b2'),
-    },
-  },
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
 });
